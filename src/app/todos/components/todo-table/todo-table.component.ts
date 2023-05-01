01e9cdd8647195d717ef275f350cb15da4c8c5be
Fatabail <tabaglio.fanny@gmail.com>
2023-05-01 23:55:56 +0200

    #2 Change a TODO state

--- src/app/todos/components/todo-table/todo-table.component.ts
+++ src/app/todos/components/todo-table/todo-table.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TodoDto } from '../../dto/todo-dto';
 import { MatTableDataSource } from '@angular/material/table';
 import { TodosService } from '../../service/todos.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-todo-table',
@@ -15,7 +16,8 @@ export class TodoTableComponent implements OnInit{
 
   todos!: TodoDto[];
   todo: TodoDto = new TodoDto();
-  displayedColumns: string[] = ['title', 'status'];
+  displayedColumns: string[] = ['title', 'status', 'select'];
+  selection = new SelectionModel<TodoDto>(true, []);
 
   constructor (private todosService: TodosService,
     private router: Router) {}
@@ -25,5 +27,21 @@ export class TodoTableComponent implements OnInit{
       console.log(data);
       this.todos = data.datas.todos;
     });
+  }
+
+  sortToDoListByStatus() {
+    this.todos.sort((a, b) => b.statusType.localeCompare(a.statusType));
+    console.log(this.todos);
+  }
+
+  onTodoToggled(todo : TodoDto) {
+    this.selection.toggle(todo);
+    const selectedTodo = this.selection.selected;
+    selectedTodo.forEach( row => {
+      row.statusType = "Done";
+      //row.title = row.title.strike();
+    }
+    )
+    console.log(selectedTodo);
   }
 }
